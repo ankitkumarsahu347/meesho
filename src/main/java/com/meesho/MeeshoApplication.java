cd5b923ed9e25d7e5b865c9f19fcca4381a437ff
ankitkumarsahu347 <ankitkumarsahu347@gmail.com>
2026-09-08 15:22:32 +0530

    Add Meesho application entry point

---
+++ src/main/java/com/meesho/MeeshoApplication.java
@@ -0,0 +1,11 @@
+package com.meesho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeeshoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MeeshoApplication.class, args);
+    }
+}
